e9d39ac6a21ffdd0ffaac5d86cf906828f9fed80
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-02-10 17:36:35 +0100

    add express

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res) {
+
+  var obj = {};
+  obj.title = 'Hello World!';
+
+  res.send(obj);
+})
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+})
